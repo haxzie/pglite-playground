d6abe9326da0e0e7f1f2ce3f0d83a43dec93030e
haxzie <musthu.gm@gmail.com>
2024-07-26 18:03:45 +0530

    Fix default SQL

--- src/components/modules/Editor/Editor.tsx
+++ src/components/modules/Editor/Editor.tsx
@@ -11,25 +11,7 @@ export default function Editor({
 }: {
   onClickRun: (query: string) => void;
 }) {
-  const [query, setQuery] = React.useState(`SELECT 
-    t.table_name,
-    c.column_name,
-    c.data_type,
-    c.character_maximum_length,
-    c.numeric_precision,
-    c.numeric_scale,
-    c.is_nullable
-FROM 
-    information_schema.tables t
-JOIN 
-    information_schema.columns c 
-    ON t.table_name = c.table_name
-WHERE 
-    t.table_schema = 'public'
-    AND t.table_type = 'BASE TABLE'
-ORDER BY 
-    t.table_name,
-    c.ordinal_position;`);
+  const [query, setQuery] = React.useState(`SELECT 'Hello World!' as MESSAGE`);
 
   // add cmd + enter to run query
   useEffect(() => {
