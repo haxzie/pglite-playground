12f425e0d0d9f18261d7db49d37a14321d5ef123
haxzie <musthu.gm@gmail.com>
2024-07-28 23:24:01 +0530

    Add tab names

--- src/components/common/EditorTopBar.tsx
+++ src/components/common/EditorTopBar.tsx
@@ -3,7 +3,7 @@ import styles from "./EditorTopBar.module.scss";
 export default function EditorTopBar() {
   return (
     <div className={styles.topBar}>
-        
+        <p className={styles.tabName}>Untitled Query</p>
     </div>
   )
 }
