beff7a90e2697f90cfefd57fe2a86a78539fe6fa
haxzie <musthu.gm@gmail.com>
2024-07-28 13:42:08 +0530

    Enable gtap

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePluginRadar } from "vite-plugin-radar";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: "G-MYZB80K2NR",
+      },
+    }),
+  ],
   optimizeDeps: {
     exclude: ["@electric-sql/pglite"],
   },
